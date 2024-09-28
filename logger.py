ef4640a2eadffdd26a46741484f3b63acf67e144
JustRomacH <romachgaming@gmail.com>
2024-09-28 13:09:59 +0300

    removed unneeded imports and vars

--- logger.py
+++ logger.py
@@ -1,33 +1,33 @@
-from timer import getTime
+from timer import get_time
 from termcolor import cprint
 
 
 def success(text: str) -> str:
-    response = f"[{getTime()}] [+] {text}"
+    response = f"[{get_time()}] [+] {text}"
     cprint(response, "green")
     return response
 
 
 def error(ex: Exception) -> str:
-    response = f"[{getTime()}] [-] {ex}"
+    response = f"[{get_time()}] [-] {ex}"
     cprint(response, "red")
     return response
 
 
 def warning(text: str) -> str:
-    response = f"[{getTime()}] [!] {text}"
+    response = f"[{get_time()}] [!] {text}"
     cprint(response, "yellow")
     return response
 
 
 def inf(text: str) -> str:
-    response = f"[{getTime()}] [~] {text}"
+    response = f"[{get_time()}] [~] {text}"
     cprint(response, "white")
     return response
 
 
 def unimportantInfo(text: str) -> str:
-    response = f"[{getTime()}] [~] {text}"
+    response = f"[{get_time()}] [~] {text}"
     cprint(response, "light_grey")
     return response
 
