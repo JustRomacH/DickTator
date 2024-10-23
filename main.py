50bfbd11c214cc8c811ce3fe6811e16e3344b2d8
JustRomacH <romachgaming@gmail.com>
2024-10-24 00:45:27 +0300

    minor fix

--- main.py
+++ main.py
@@ -149,6 +149,6 @@ if __name__ == "__main__":
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        logging.info("Bot disabled..")
+        logging.info("Bot disabled...")
     except Exception as exc:
         logging.error(exc)
