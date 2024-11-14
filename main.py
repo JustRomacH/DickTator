729472098a87c62928499dc5fb204d993e6a46cf
JustRomacH <romachgaming@gmail.com>
2024-11-14 11:20:33 +0300

    added extra tabs

--- main.py
+++ main.py
@@ -185,7 +185,9 @@ async def main() -> None:
 if __name__ == "__main__":
     try:
         asyncio.run(main())
+
     except KeyboardInterrupt:
         logging.info("Bot disabled...")
+
     except Exception as exc:
         logging.error(exc)
